139de94536aaf59a7fd6364d26e9c325ba2df747
Bryce Miller <bryce@brycem.co>
2022-07-26 21:26:49 -0400

    cell selection

--- src/Pages/Solve.elm
+++ src/Pages/Solve.elm
@@ -3,12 +3,14 @@ module Pages.Solve exposing (Model, Msg, page)
 import Gen.Params.Solve exposing (Params)
 import Gen.Route as Route
 import Html exposing (Html, div, table, td, text, tr)
-import Html.Attributes exposing (class)
+import Html.Attributes exposing (class, classList)
+import Html.Events exposing (onClick)
+import Html.Lazy exposing (lazy2)
 import Page
 import Request
 import Shared
 import Sudoku.Cell as Cell exposing (Cell)
-import Sudoku.Grid as Grid exposing (Grid)
+import Sudoku.Grid as Grid exposing (Coord, Grid)
 import UI
 import View exposing (View)
 
@@ -28,19 +30,21 @@ page shared req =
 
 
 type alias Model =
-    { puzzle : Grid }
+    { puzzle : Grid
+    , selectedCell : Maybe Coord
+    }
 
 
 init : Request.With Params -> Maybe Grid -> ( Model, Cmd Msg )
 init req puzzle =
     case puzzle of
         Nothing ->
-            ( { puzzle = Grid.empty }
+            ( { puzzle = Grid.empty, selectedCell = Nothing }
             , Request.pushRoute Route.Home_ req
             )
 
         Just grid ->
-            ( { puzzle = grid }
+            ( { puzzle = grid, selectedCell = Nothing }
             , Cmd.none
             )
 
@@ -50,14 +54,16 @@ init req puzzle =
 
 
 type Msg
-    = ReplaceMe
+    = ClickedCell Coord
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        ReplaceMe ->
-            ( model, Cmd.none )
+        ClickedCell coord ->
+            ( { model | selectedCell = Just coord }
+            , Cmd.none
+            )
 
 
 
@@ -76,15 +82,50 @@ subscriptions model =
 view : Model -> View Msg
 view model =
     let
-        viewCell index cell =
+        viewCell :
+            { currentCoord : Coord
+            , selectedCoord : Maybe Coord
+            }
+            -> Cell
+            -> Html Msg
+        viewCell { currentCoord, selectedCoord } cell =
+            let
+                { x, y } =
+                    currentCoord
+
+                selected =
+                    case selectedCoord of
+                        Just coord ->
+                            x == coord.x && y == coord.y
+
+                        Nothing ->
+                            False
+
+                twClasses =
+                    "h-14 w-14 flex justify-center items-center text-3xl"
+            in
             td [ class "border border-zinc-700" ]
                 [ div
-                    [ class "h-14 w-14 flex justify-center items-center text-3xl" ]
+                    [ classList
+                        [ ( twClasses ++ " selected", selected )
+                        , ( twClasses, not selected )
+                        ]
+                    , onClick <| ClickedCell currentCoord
+                    ]
                     [ text (Cell.numberToString cell) ]
                 ]
 
-        viewRow index row =
-            tr [] (List.indexedMap viewCell row)
+        viewRow : Int -> List Cell -> Html Msg
+        viewRow y row =
+            tr [] <|
+                List.indexedMap
+                    (\x ->
+                        lazy2 viewCell
+                            { currentCoord = { x = x, y = y }
+                            , selectedCoord = model.selectedCell
+                            }
+                    )
+                    row
     in
     { title = "Solve"
     , body =
