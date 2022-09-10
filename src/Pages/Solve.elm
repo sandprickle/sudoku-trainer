4b7567544dc3119345712c695099d57ef286605a
Bryce Miller <sandprickle@users.noreply.github.com>
2022-09-10 11:56:09 -0400

    keyboard navigation

--- src/Pages/Solve.elm
+++ src/Pages/Solve.elm
@@ -64,54 +64,112 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         ClickedCell coord ->
-            ( { model | selectedCell = Just coord }
-            , Cmd.none
-            )
+            ( { model | selectedCell = Just coord }, Cmd.none )
 
         KeyDown rawKey ->
-            let
-                keyStr =
-                    case Keyboard.characterKeyOriginal rawKey of
-                        Just (Keyboard.Character str) ->
-                            str
-
-                        _ ->
-                            ""
-            in
-            Debug.todo "handle keyboard input"
+            handleKeyboardInput rawKey model
 
 
+handleKeyboardInput : RawKey -> Model -> ( Model, Cmd Msg )
+handleKeyboardInput rawKey model =
+    let
+        keyStr =
+            case Keyboard.characterKeyOriginal rawKey of
+                Just (Keyboard.Character str) ->
+                    str
 
--- valueKeys : Set String
--- valueKeys =
---     Set.fromList [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" ]
--- navKeys : Set String
--- navKeys =
---     Set.fromList [ "h", "j", "k", "l" ]
--- moveSelectionLeft : Coord -> Coord
--- moveSelectionLeft { x, y } =
---     if x == 0 then
---         { x = 8, y = y }
---     else
---         { x = x - 1, y = y }
--- moveSelectionRight : Coord -> Coord
--- moveSelectionRight { x, y } =
---     if x == 8 then
---         { x = 0, y = y }
---     else
---         { x = x + 1, y = y }
--- moveSelectionUp : Coord -> Coord
--- moveSelectionUp { x, y } =
---     if y == 0 then
---         { x = x, y = 8 }
---     else
---         { x = x, y = y - 1 }
--- moveSelectionDown : Coord -> Coord
--- moveSelectionDown { x, y } =
---     if y == 8 then
---         { x = x, y = 0 }
---     else
---         { x = x, y = y + 1 }
+                _ ->
+                    ""
+    in
+    if Set.member keyStr valueKeys then
+        case model.selectedCell of
+            Just coord ->
+                ( model, Cmd.none )
+
+            Nothing ->
+                ( model, Cmd.none )
+
+    else if Set.member keyStr navKeys then
+        case model.selectedCell of
+            Nothing ->
+                ( { model | selectedCell = Just { x = 4, y = 4 } }, Cmd.none )
+
+            Just coord ->
+                case keyStr of
+                    "h" ->
+                        ( { model | selectedCell = Just (moveSelectionLeft coord) }
+                        , Cmd.none
+                        )
+
+                    "j" ->
+                        ( { model | selectedCell = Just (moveSelectionDown coord) }
+                        , Cmd.none
+                        )
+
+                    "k" ->
+                        ( { model | selectedCell = Just (moveSelectionUp coord) }
+                        , Cmd.none
+                        )
+
+                    "l" ->
+                        ( { model | selectedCell = Just (moveSelectionRight coord) }
+                        , Cmd.none
+                        )
+
+                    _ ->
+                        ( model, Cmd.none )
+
+    else
+        ( model, Cmd.none )
+
+
+valueKeys : Set String
+valueKeys =
+    Set.fromList [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" ]
+
+
+navKeys : Set String
+navKeys =
+    Set.fromList [ "h", "j", "k", "l" ]
+
+
+moveSelectionLeft : Coord -> Coord
+moveSelectionLeft { x, y } =
+    if x == 0 then
+        { x = 8, y = y }
+
+    else
+        { x = x - 1, y = y }
+
+
+moveSelectionRight : Coord -> Coord
+moveSelectionRight { x, y } =
+    if x == 8 then
+        { x = 0, y = y }
+
+    else
+        { x = x + 1, y = y }
+
+
+moveSelectionUp : Coord -> Coord
+moveSelectionUp { x, y } =
+    if y == 0 then
+        { x = x, y = 8 }
+
+    else
+        { x = x, y = y - 1 }
+
+
+moveSelectionDown : Coord -> Coord
+moveSelectionDown { x, y } =
+    if y == 8 then
+        { x = x, y = 0 }
+
+    else
+        { x = x, y = y + 1 }
+
+
+
 -- SUBSCRIPTIONS
 
 
