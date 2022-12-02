862a5409e40523d53b73b90ad37015d521ceeee2
Bryce Miller <sandprickle@users.noreply.github.com>
2022-12-02 13:22:17 -0500

    refactor to use Sudoku.Grid module

--- src/Sudoku/SolveGrid.elm
+++ src/Sudoku/SolveGrid.elm
@@ -7,13 +7,11 @@ port module Sudoku.SolveGrid exposing
     , encode
     , encodeCoord
     , fromJson
-    , fromString
     , getByCoord
     , isLegal
     , isSolvable
     , legal
     , load
-    , preview
     , pruneAll
     , receiver
     , resetPossible
@@ -32,26 +30,12 @@ import Json.Decode as Decode exposing (Decoder)
 import Json.Encode as Encode
 import List.Extra
 import Sudoku.Cell as Cell exposing (Cell(..))
+import Sudoku.Grid as Grid exposing (Grid)
 import Sudoku.Number as Number exposing (NumSet)
 
 
-type SolveGrid
-    = SolveGrid (Array Cell)
-
-
-
--- New SolveGrid from String
-
-
-fromString : String -> SolveGrid
-fromString str =
-    String.trim str
-        |> String.left 81
-        |> String.padRight 81 '.'
-        |> String.toList
-        |> List.map Cell.initFromChar
-        |> Array.fromList
-        |> SolveGrid
+type alias SolveGrid =
+    Grid Cell
 
 
 
@@ -59,13 +43,9 @@ fromString str =
 
 
 solvable : SolveGrid -> Maybe SolveGrid
-solvable (SolveGrid grid) =
-    let
-        givenNumbers =
-            Array.filter Cell.isGiven grid
-    in
-    if Array.length givenNumbers >= 17 then
-        Just (SolveGrid grid)
+solvable grid =
+    if isSolvable grid then
+        Just grid
 
     else
         Nothing
@@ -73,40 +53,12 @@ solvable (SolveGrid grid) =
 
 isSolvable : SolveGrid -> Bool
 isSolvable grid =
-    solvable grid /= Nothing
+    Grid.isSolvable Cell.getNumber grid
 
 
 legal : SolveGrid -> Maybe SolveGrid
 legal grid =
-    let
-        checkGroup : List Cell -> Bool
-        checkGroup group =
-            group
-                |> List.filterMap Cell.getNumber
-                |> List.Extra.allDifferent
-
-        rowsOk =
-            grid
-                |> toRows
-                |> List.map checkGroup
-                |> List.member False
-                |> not
-
-        colsOk =
-            grid
-                |> toCols
-                |> List.map checkGroup
-                |> List.member False
-                |> not
-
-        boxesOk =
-            grid
-                |> toBoxes
-                |> List.map checkGroup
-                |> List.member False
-                |> not
-    in
-    if rowsOk && colsOk && boxesOk then
+    if isLegal grid then
         Just grid
 
     else
@@ -115,7 +67,7 @@ legal grid =
 
 isLegal : SolveGrid -> Bool
 isLegal grid =
-    legal grid /= Nothing
+    Grid.isLegal Cell.getNumber grid
 
 
 
@@ -123,13 +75,13 @@ isLegal grid =
 
 
 getByCoord : Coord -> SolveGrid -> Cell
-getByCoord coord (SolveGrid grid) =
-    Array.get (coordToIndex coord) grid |> Maybe.withDefault Cell.default
+getByCoord =
+    Grid.getByCoord
 
 
 setByCoord : Coord -> SolveGrid -> Cell -> SolveGrid
-setByCoord coord (SolveGrid grid) newCell =
-    Array.set (coordToIndex coord) newCell grid |> SolveGrid
+setByCoord =
+    Grid.setByCoord
 
 
 
@@ -137,37 +89,18 @@ setByCoord coord (SolveGrid grid) newCell =
 
 
 toRows : SolveGrid -> List (List Cell)
-toRows grid =
-    List.map (\n -> getRow n grid) (List.range 0 8)
+toRows =
+    Grid.toRows
 
 
 toCols : SolveGrid -> List (List Cell)
-toCols grid =
-    List.map (\n -> getCol n grid) (List.range 0 8)
+toCols =
+    Grid.toCols
 
 
 toBoxes : SolveGrid -> List (List Cell)
-toBoxes grid =
-    List.map (\n -> getBox n grid) (List.range 0 8)
-
-
-
--- Get a Specific row, col, or box as a List
-
-
-getRow : Int -> SolveGrid -> List Cell
-getRow rowNum grid =
-    List.map (\coord -> getByCoord coord grid) (rowCoords rowNum)
-
-
-getCol : Int -> SolveGrid -> List Cell
-getCol colNum grid =
-    List.map (\coord -> getByCoord coord grid) (colCoords colNum)
-
-
-getBox : Int -> SolveGrid -> List Cell
-getBox boxNum grid =
-    List.map (\coord -> getByCoord coord grid) (boxCoords boxNum)
+toBoxes =
+    Grid.toBoxes
 
 
 
@@ -175,45 +108,18 @@ getBox boxNum grid =
 
 
 rowCoords : Int -> List Coord
-rowCoords rowNum =
-    let
-        getCoord i =
-            { x = i, y = rowNum }
-    in
-    List.map getCoord (List.range 0 8)
+rowCoords =
+    Grid.rowCoords
 
 
 colCoords : Int -> List Coord
-colCoords colNum =
-    let
-        getCoord i =
-            { x = colNum, y = i }
-    in
-    List.map getCoord (List.range 0 8)
+colCoords =
+    Grid.colCoords
 
 
 boxCoords : Int -> List Coord
-boxCoords boxNum =
-    let
-        boxRow =
-            boxNum // 3
-
-        boxCol =
-            modBy 3 boxNum
-
-        yCoords =
-            List.map (\n -> (3 * boxRow) + n) (List.range 0 2)
-
-        xCoords =
-            List.map (\n -> (3 * boxCol) + n) (List.range 0 2)
-
-        getCoord y x =
-            { x = x, y = y }
-
-        coordsInRow yCoord =
-            List.map (getCoord yCoord) xCoords
-    in
-    List.map coordsInRow yCoords |> List.concat
+boxCoords =
+    Grid.boxCoords
 
 
 
@@ -302,7 +208,7 @@ pruneCells cells =
 
 empty : SolveGrid
 empty =
-    Array.fromList (List.repeat 81 Cell.default) |> SolveGrid
+    Grid.init Cell.default
 
 
 
@@ -310,9 +216,9 @@ empty =
 
 
 resetPossible : SolveGrid -> SolveGrid
-resetPossible (SolveGrid arr) =
-    arr
-        |> Array.map
+resetPossible grid =
+    grid
+        |> Grid.map
             (\cell ->
                 case cell of
                     Fixed _ _ ->
@@ -324,7 +230,6 @@ resetPossible (SolveGrid arr) =
                     Possible _ notes ->
                         Possible Number.setAll notes
             )
-        |> SolveGrid
 
 
 
@@ -332,13 +237,13 @@ resetPossible (SolveGrid arr) =
 
 
 encode : SolveGrid -> Encode.Value
-encode (SolveGrid grid) =
-    Encode.array Cell.encode grid
+encode =
+    Grid.encode Cell.encode
 
 
 decoder : Decoder SolveGrid
 decoder =
-    Decode.map SolveGrid (Decode.array Cell.decoder)
+    Grid.decoder Cell.decoder
 
 
 fromJson : Encode.Value -> Maybe SolveGrid
@@ -384,76 +289,14 @@ receiver fromGrid =
 
 
 type alias Coord =
-    { x : Int, y : Int }
-
-
-coordToIndex : Coord -> Int
-coordToIndex coord =
-    let
-        { x, y } =
-            normalizeCoord coord
-    in
-    (y * 9) + x
-
-
-normalizeCoord : Coord -> Coord
-normalizeCoord coord =
-    let
-        y =
-            if coord.y > 8 then
-                8
-
-            else if coord.y < 0 then
-                0
-
-            else
-                coord.y
-
-        x =
-            if coord.x > 8 then
-                8
-
-            else if coord.x < 0 then
-                0
-
-            else
-                coord.x
-    in
-    { x = x, y = y }
+    Grid.Coord
 
 
 encodeCoord : Coord -> Encode.Value
-encodeCoord coord =
-    Encode.object
-        [ ( "x", Encode.int coord.x )
-        , ( "y", Encode.int coord.y )
-        ]
+encodeCoord =
+    Grid.encodeCoord
 
 
 coordDecoder : Decoder Coord
 coordDecoder =
-    Decode.map2 Coord
-        (Decode.field "x" Decode.int)
-        (Decode.field "y" Decode.int)
-
-
-
--- View Helpers
-
-
-preview : SolveGrid -> Html msg
-preview grid =
-    let
-        rows =
-            toRows grid
-
-        viewCell cell =
-            Html.td []
-                [ Html.div [] [ Html.text (Cell.numberToString cell) ] ]
-
-        viewRow row =
-            Html.tr [] (List.map viewCell row)
-    in
-    Html.table
-        [ Html.Attributes.class "puzzle-preview border-2 border-gray-400" ]
-        (List.map viewRow rows)
+    Grid.coordDecoder
